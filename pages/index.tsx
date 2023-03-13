b8feed8fb3f2e6ae87ffdfd39452dcd33279ff23
siddiq <msiddiq1400@gmail.com>
2023-03-13 13:41:56 +0500

    Navigation via Link from Next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
+import Link from 'next/link'
+
 function Home() {
-  return <h1>Hello 2</h1>
+  return (
+    <>
+      <h1>Hello 2</h1>
+      <p>
+        <Link href="/blog">Blog</Link>
+      </p>
+      <p>
+        <Link href="/product">Products</Link>
+      </p>
+    </>
+  )
 }
 export default Home
--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,9 +1,22 @@
-function ProductList() {
+import Link from 'next/link'
+
+function ProductList({ productId = 100 }) {
   return (
     <>
-      <h2>Product 1</h2>
-      <h2>Product 1</h2>
-      <h2>Product 1</h2>
+      <p><Link href="/">Home</Link></p>
+      <h2><Link href="/product/1">Product 1</Link></h2>
+
+      {/* replace clear history */}
+      <h2><Link href="/product/2" replace>product 2</Link></h2>
+
+      <h2><Link href="/product/3">Product 3</Link></h2>
+      <h2>
+        <Link href={`/product/${productId}`}>
+          Product
+          {' '}
+          {productId}
+        </Link>
+      </h2>
     </>
   )
 }
