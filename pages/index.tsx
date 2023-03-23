bdec6e518fd2903bfae7c3dbea5ff9e8574ade1d
siddiq <msiddiq1400@gmail.com>
2023-03-24 03:22:17 +0500

    data pre fetched on home page for users

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 function Home() {
   return (
     <>
       <h1>Nextjs Pre Rendering</h1>
+      <p>
+        <Link href="/users">Users</Link>
+      </p>
     </>
   )
 }
